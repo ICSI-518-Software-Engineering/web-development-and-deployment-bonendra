42e03f6477de8a22909cd852e1d84721368aa1ab
bonendra <bkandati@albany.edu>
2024-02-25 20:34:52 +0530

    Final

--- my-app/src/Operations.js
+++ my-app/src/Operations.js
@@ -22,7 +22,7 @@ const Operations=()=>
         var val2=parseInt(inp2.current.value,10);
         console.log(val1,val2);
 
-        fetch("http://us-east-2.console.aws.amazon.com/ec2/home?region=us-east-2#Instances:instanceId=i-06f80ca7a6363fbcc:8080/addition",
+        fetch("http://ec2-3-15-16-47.us-east-2.compute.amazonaws.com:8080/addition",
         {
             method:'POST',
             headers:{"Content-Type":"application/json"},
